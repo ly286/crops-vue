c08fa456fd17b9099d03b3e0d8338499c7b153c3
ly286 <dlylove286@gmail.com>
2024-03-08 21:13:54 +0800

    首次提交
    1.导入elementui与axios

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
@@ -28,3 +28,11 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+
+<script lang="ts" setup></script>
+<!-- <script>
+  import { ElButton } from 'element-plus'
+  export default {
+    components: { ElButton },
+  }
+</script> -->
--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import axios from 'axios';
 
-createApp(App).use(store).use(router).mount('#app')
+
+// 将 Axios 添加到 Vue 实例的原型链上
+const app = createApp(App);
+app.config.globalProperties.$axios = axios;
+
+createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
+
